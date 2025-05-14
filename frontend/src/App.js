3b8c2af07cc6d59ca8272905216bd4b1fda0686f
bysejin <81725570+bysejin@users.noreply.github.com>
2025-05-15 02:46:36 +0900

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RoomVisualizer from './components/RoomVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <RoomVisualizer />
+    </div>
+  );
+}
+
+export default App;
